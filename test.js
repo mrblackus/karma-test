00b2116d31e47786505c2f3de959ff466232a6d0
Mathieu Savy <mathieu.savy@gmail.com>
2015-12-11 15:23:10 +0100

    Correct unit test for minus method

--- test.js
+++ test.js
@@ -13,11 +13,11 @@ describe('Main file', function () {
   describe('minus function', function () {
 
     it('should return an integer', function () {
-      expect(plus(1, 2)).to.be.a('number');
+      expect(minus(1, 2)).to.be.a('number');
     });
 
     it('should return a correct value', function () {
-      expect(plus(1, 2)).to.be.equal(-1);
+      expect(minus(1, 2)).to.be.equal(-1);
     });
   });
 });
